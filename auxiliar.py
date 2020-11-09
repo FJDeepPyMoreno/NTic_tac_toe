c3560a86fae787ee77970abf6e9884346ac79b7b
Paco_MorenoMadrid <paco.jalcama@gmail.com>
2020-11-09 17:59:00 +0100

    wrong change in auxiliar

--- auxiliar.py
+++ auxiliar.py
@@ -3,3 +3,12 @@ def sum_strings(str_list):
     for i in str_list:
         sum_st += i
     return sum_st
+
+
+# This is a function that has to be removed:
+
+def wrong(stri):
+    if stri > 2:
+        print("Nothing to do")
+
+
